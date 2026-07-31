a7f2f45939510ec434d4cdb84f5c0e513d982f4b
jijuuhuju <ryota.summer7@icloud.com>
2026-07-31 10:13:28 +0900

    Update engine.js

--- engine.js
+++ engine.js
@@ -1,88 +1,60 @@
-const code = localStorage.getItem("pscaCode");
+async function runPsca() {
 
-const stage = document.getElementById("stage");
+    while (i < lines.length) {
 
-if (!code) {
-    alert("Pscaコードがありません");
-}
+        let line = lines[i].trim();
 
-const lines = code.split("\n");
-
-let i = 0;
-
-while (i < lines.length) {
-
-    let line = lines[i].trim();
-
-    // START
-    if (line === "START") {
-        i++;
-        continue;
-    }
-
-    // END
-    if (line === "END") {
-        break;
-    }
-
-
-    // print()
-    if (line.startsWith("print")) {
-
-        let text = line
-            .replace('print("', "")
-            .replace('")', "");
-
-        const p = document.createElement("p");
-        p.textContent = text;
-
-        stage.appendChild(p);
-    }
-
-
-    // alert()
-    if (line.startsWith("alert")) {
-
-        let text = line
-            .replace('alert("', "")
-            .replace('")', "");
-
-        alert(text);
-    }
-
-
-    // BUTTON
-    if (line === "BUTTON") {
-
-        i++;
-
-        let name = "";
-        let id = "";
-
-        while (i < lines.length && lines[i].trim() !== "") {
-
-            let part = lines[i].trim();
-
-            if (part.startsWith("name")) {
-                name = part.split("=")[1].trim().replaceAll('"',"");
-            }
-
-            if (part.startsWith("id")) {
-                id = part.split("=")[1].trim().replaceAll('"',"");
-            }
-
+        if (line === "START") {
             i++;
+            continue;
+        }
+
+        if (line === "END") {
+            break;
         }
 
 
-        let button = document.createElement("button");
+        if (line.startsWith("print")) {
 
-        button.textContent = name;
-        button.id = id;
+            let text = line
+                .replace('print("', "")
+                .replace('")', "");
 
-        stage.appendChild(button);
+            const p = document.createElement("p");
+            p.textContent = text;
+
+            stage.appendChild(p);
+
+            // 表示を反映させる
+            await new Promise(resolve => setTimeout(resolve, 0));
+        }
+
+
+        if (line.startsWith("wait")) {
+
+            let seconds = Number(
+                line.replace("wait ", "")
+                    .replace(" seconds", "")
+            );
+
+            await new Promise(resolve =>
+                setTimeout(resolve, seconds * 1000)
+            );
+        }
+
+
+        if (line.startsWith("alert")) {
+
+            let text = line
+                .replace('alert("', "")
+                .replace('")', "");
+
+            alert(text);
+        }
+
+
+        i++;
     }
-
-
-    i++;
 }
+
+runPsca();
